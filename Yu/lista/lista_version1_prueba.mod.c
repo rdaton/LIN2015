5bf31ed861863fd002d694ee1761c1fdd2126a8b
yuliu03 <yuliu03@ucm.es>
2016-10-08 17:28:40 +0200

    Add files via upload

---
+++ Yu/lista/lista_version1_prueba.mod.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xdca08a4c, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x8819b6c1, __VMLINUX_SYMBOL_STR(remove_proc_entry) },
+	{ 0x245505fd, __VMLINUX_SYMBOL_STR(proc_create_data) },
+	{ 0x999e8297, __VMLINUX_SYMBOL_STR(vfree) },
+	{ 0xc72e1233, __VMLINUX_SYMBOL_STR(__trace_bprintk) },
+	{ 0xe2d5255a, __VMLINUX_SYMBOL_STR(strcmp) },
+	{ 0x2d41e6f5, __VMLINUX_SYMBOL_STR(__trace_puts) },
+	{ 0x20c55ae0, __VMLINUX_SYMBOL_STR(sscanf) },
+	{ 0xb1e25684, __VMLINUX_SYMBOL_STR(__trace_bputs) },
+	{ 0xd6ee688f, __VMLINUX_SYMBOL_STR(vmalloc) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
