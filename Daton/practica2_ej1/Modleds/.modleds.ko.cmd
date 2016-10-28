c2fb52ca02744ed34654ebc37246ef3e286f68ea
kernel <kernel@debian>
2016-10-28 15:34:15 +0200

    sep

---
+++ Daton/practica2_ej1/Modleds/.modleds.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kernel/LIN2015/Daton/practica2_ej1/Modleds/modleds.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.14.1.lin/scripts/module-common.lds --build-id  -o /home/kernel/LIN2015/Daton/practica2_ej1/Modleds/modleds.ko /home/kernel/LIN2015/Daton/practica2_ej1/Modleds/modleds.o /home/kernel/LIN2015/Daton/practica2_ej1/Modleds/modleds.mod.o
