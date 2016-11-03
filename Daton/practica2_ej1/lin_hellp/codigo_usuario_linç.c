289b5ce3aadf8d82fbdec9bc822542a66c7dd2c0
kernel <kernel@debian>
2016-11-03 19:45:46 +0100

    nuevoLed

--- Daton/practica2_ej1/lin_hellp/codigo_usuario_linç.c
+++ Daton/practica2_ej1/lin_hellp/codigo_usuario_linç.c
@@ -1,14 +1,12 @@
 #include <linux/errno.h>
 #include <sys/syscall.h>
 #include <linux/unistd.h>
-#include<stdio.h>
-#ifdef__i386__
-#define
-__NR_HELLO 353
-#else
+#include <stdio.h>
+
+
 #define
 __NR_HELLO 316
-#endif
+
 long lin_hello(void) 
 {
   return(long) syscall(__NR_HELLO);
